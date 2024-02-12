299017439638a2a017dccc46a04ee2b64f02a888
zougataga <107312482+zougataga@users.noreply.github.com>
2024-02-12 13:10:17 +0100

    Update index.js

--- src/lib/db/index.js
+++ src/lib/db/index.js
@@ -1,5 +1,5 @@
 import zougatagaDb from 'zougatagadb';
 export default new zougatagaDb({
     cryptData: false,
-    path: __dirname+'\\src\\lib\\db\\data.json' // .db
+    path: process.cwd() + '/src/lib/db/data.json' // .db
 })
