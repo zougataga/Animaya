9210c10f396c3b85ef9c92f86cc9a13250084220
zougataga <107312482+zougataga@users.noreply.github.com>
2024-02-12 12:26:19 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -154,7 +154,7 @@ export default function ({
                                     <Link className='dropdown-item' href={`/user/list`}>
                                         <div className='d-flex gap-2'>
                                             <div className='d-flex align-items-center justify-content-center'>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M4 14m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /></svg>                                            </div>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /><path d="M4 14m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" /></svg>                                            </div>
                                             <div className='d-flex flex-column'>
                                                 <h3 className='m-0 p-0'>Mes listes</h3>
                                                 <p className='m-0 p-0 text-secondary' >Voir vos listes</p>
